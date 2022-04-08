d00cc0fc82a783bd5e60d0992bcf4c8e8fe418be
esby <seabass.branch@gmail.com>
2022-04-08 19:42:33 -0400

    OMG IT KINDA WORKS

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ def main() -> None:
 
         if k % 256 == 32:
             # SPACE pressed
-            color = get_color(img, start_point, end_point)
-            print(color)
+            colors = get_face_colors(img, start_point, end_point)
+            print(colors)
 
         img = draw_cube(img, start_point, end_point)
         cv.imshow("esby rubik's cube solver :D", img)
@@ -57,16 +57,28 @@ def draw_cube(img, start: Tuple[int, int], end: Tuple[int, int]):
     return img
 
 
-def get_color(img, start: Tuple[int, int], end: Tuple[int, int]) -> str:
-    """Get rubik's cube color from given image"""
+def get_face_colors(img, start: Tuple[int, int], end: Tuple[int, int]) -> str:
+    """Gets all the colors from a face of the rubik's cube"""
     start_x, start_y = start
     end_x, end_y = end
-    img = img[start_y:end_y, start_x:end_x]
+    dif_x = math.ceil((end_x - start_x) / 3)
+    dif_y = math.ceil((end_y - start_y) / 3)
 
-    color_values = get_color_values(img, True)
+    color_string = ""
+    for i in range(start_y, end_y, dif_y):
+        for j in range(start_x, end_x, dif_x):
+            color_symbol = get_color(img[i : i + dif_y, j : j + dif_x])[0].upper()
+            color_string += color_symbol
 
+    return color_string
+
+
+def get_color(img) -> str:
+    """Gets an images main color from a given image"""
+    color_values = get_color_values(img)
     main_color: None | str = None
     largest_value = 0
+
     for color, value in color_values.items():
         if value > largest_value:
             main_color = color
@@ -76,6 +88,7 @@ def get_color(img, start: Tuple[int, int], end: Tuple[int, int]) -> str:
 
 
 def get_color_values(img, show_windows=False) -> Dict[str, int]:
+    """Gets the color from a given image"""
     hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     color_values: Dict[str, int] = {}
 
